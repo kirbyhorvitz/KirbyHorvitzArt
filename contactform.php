ce49e3969fd1a5d08810eebdecdc3a277a0eafbd
kirbyhorvitz <kth49@georgetown.edu>
2021-04-18 21:45:56 -0400

    Update contactform.php

---
+++ contactform.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+Thank you for reaching out, <?php echo $_POST["Name"]; ?><br>
+The email address you submitted is: <?php echo $_POST["Email"]; ?><br>
+The message you submitted is: <?php echo $_POST["Message"]; ?>
+</body>
+</html>
